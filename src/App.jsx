9da3ebbe29d4bc10da24d6fbb4d86522272b53e0
codejocker <vibekillos1234@gmail.com>
2026-08-27 18:52:56 +0200

    feat(ui): add and configure the fonts that are to be used

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-      <h1 className='text-xl font-bold text-blue-600'>cj snap here for you, hey are you </h1>
+      <h1 className='text-xl font-jet text-blue-600'>cj snap here for you, hey are you </h1>
     </div>
   )
 }
